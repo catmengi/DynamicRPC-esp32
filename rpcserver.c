61ade99e68f08515b2f06e7013d186757cfd8848
Catmengi] <catengineerforever@gmail.com>
2024-08-04 17:05:02 +0300

    make socket blocking

--- rpcserver.c
+++ rpcserver.c
@@ -113,12 +113,6 @@ struct rpcserver* rpcserver_create(uint16_t port){
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-    int status = fcntl(server_fd, F_SETFL, fcntl(server_fd, F_GETFL, 0) | O_NONBLOCK);
-
-    if (status == -1){
-    perror("calling fcntl");
-    // handle the error.  By the way, I've never seen fcntl fail in this way
-    }
     if (setsockopt(server_fd, SOL_SOCKET,
                 SO_REUSEADDR, &opt,
                 sizeof(opt))) {
@@ -174,6 +168,7 @@ void rpcserver_free(struct rpcserver* serv){
     serv->interfunc = NULL;
     free(serv->reliverargs);
     pthread_mutex_unlock(&serv->edit);
+    shutdown(serv->sfd,SHUT_RD);
     close(serv->sfd);
     pthread_join(serv->reliver,NULL);
     free(serv);
@@ -610,10 +605,10 @@ void rpcserver_dispatcher(void* vserv){
     serv->reliverargs[1] = &fd;
     socklen_t addrlen = 0;
     printf("%s: dispatcher started\n",__PRETTY_FUNCTION__);
-    while(serv->stop == 0){
+    while(1){
         if(serv->clientcount < DEFAULT_MAXIXIMUM_CLIENT){
             fd = accept(serv->sfd, (struct sockaddr*)&addr,&addrlen);
-                if(fd < 0) {vTaskDelay(pdMS_TO_TICKS((150)));continue;}
+                if(fd < 0) break;
                 printf("%s: got client from %s\n",__PRETTY_FUNCTION__,inet_ntoa(addr.sin_addr));
                 serv->is_incon = 1;
                 struct rpcmsg msg = {0};
