36ad5cce92756682f7f2c4063f4a314ba3c25eba
Vladislav <138163939+catmengi@users.noreply.github.com>
2024-08-07 11:19:52 +0300

    Update rpcclient.h
    Fixed non updated header

--- rpcclient.h
+++ rpcclient.h
@@ -7,5 +7,5 @@ struct rpccon{
 };
 
 int rpcserver_connect(char* host,char* key,int portno,struct rpccon* con);
-int rpcclient_call(struct rpccon* con,char* fn,enum rpctypes* rpctypes,char* flags, int rpctypes_len,struct rpcret* fnret,...);
+int rpcclient_call(struct rpccon* con,char* fn,enum rpctypes* rpctypes,char* flags, int rpctypes_len,void* fnret,...);
 void rpcclient_discon(struct rpccon* con);
