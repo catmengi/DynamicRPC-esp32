aa37af293cd02474cde54e43a11966e2f2e9ba15
Catmengi <138163939+catmengi@users.noreply.github.com>
2024-08-11 17:36:18 +0300

    Update rpcserver.c
    Possible memory leak! Now functions cant return rpcbuff and rpcstruct

--- rpcserver.c
+++ rpcserver.c
@@ -360,7 +360,7 @@ int __rpcserver_call_fn(struct rpcret* ret,struct rpcserver* serv,struct rpccall
             ret->ret.type = VOID;
         }
 
-        if(rtype == RPCBUFF && *(void**)fnret != NULL){
+        /*if(rtype == RPCBUFF && *(void**)fnret != NULL){
             create_rpcbuff_type(*(struct rpcbuff**)fnret,0,&ret->ret);
         }
         else if(rtype == RPCBUFF && *(void**)fnret == NULL)
@@ -370,6 +370,7 @@ int __rpcserver_call_fn(struct rpcret* ret,struct rpcserver* serv,struct rpccall
         }
         else if(rtype == RPCSTRUCT && *(void**)fnret == NULL)
             ret->ret.type = VOID;
+        */
     }
     for(uint8_t i = 0; i < ret->resargs_amm; i++){
         if(ret->resargs[i].type == RPCBUFF){
