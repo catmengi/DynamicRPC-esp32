8f6e53eae855cf64d05237b6e70279ff9fed347f
Catmengi <138163939+catmengi@users.noreply.github.com>
2024-10-18 17:35:46 +0300

    Update rpcserver.h

--- rpcserver.h
+++ rpcserver.h
@@ -3,10 +3,14 @@
 #include <pthread.h>
 #include <stdatomic.h>
 #include "freertos/idf_additions.h"
+#include <arpa/inet.h>
 #include <stddef.h>
 #include "rpccall.h"
 #include <ffi.h>
 #include "hashtable.c/hashtable.h"
+
+typedef void (*rpcserver_newclient_cb)(void* userdata, struct sockaddr_in);
+typedef void (*rpcserver_clientdiscon_cb)(void* userdata, struct sockaddr_in);
 struct rpcserver{
     int sfd;
     int stop;
@@ -16,6 +20,10 @@ struct rpcserver{
     hashtable_t users;
     atomic_ullong clientcount;
     void* interfunc;
+    void* newclient_cb_data;
+    rpcserver_newclient_cb newclient_cb;
+    void* clientdiscon_cb_data;
+    rpcserver_clientdiscon_cb clientdiscon_cb;
 };
 struct fn{
     char* fn_name;
@@ -32,6 +40,7 @@ struct client_thread{
     int client_fd;
     struct rpcserver* serv;
     char client_uniq[65];
+    struct sockaddr_in addr;
 };
 
 
@@ -43,5 +52,7 @@ void rpcserver_stop(struct rpcserver* serv);
 int rpcserver_register_fn(struct rpcserver* serv, void* fn, char* fn_name,
                           enum rpctypes rtype, enum rpctypes* argstype,
                           uint8_t argsamm, void* pstorage,int perm);
+void rpcserver_register_newclient_cb(struct rpcserver* serv,rpcserver_newclient_cb callback, void* user);
+void rpcserver_register_clientdiscon_cb(struct rpcserver* serv,rpcserver_clientdiscon_cb callback, void* user);
 void rpcserver_unregister_fn(struct rpcserver* serv, char* fn_name);
 void rpcserver_load_keys(struct rpcserver* serv, char* filename);
